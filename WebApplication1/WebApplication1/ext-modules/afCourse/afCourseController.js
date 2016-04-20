482b8e9674d9807017697734e0dc06ebdb9a2081
Paul Sausage <luc.pitipuis@gmail.com>
2016-04-20 23:06:51 +0900

    Add working kanji detection for learning, and drawing

--- WebApplication1/WebApplication1/ext-modules/afCourse/afCourseController.js
+++ WebApplication1/WebApplication1/ext-modules/afCourse/afCourseController.js
@@ -35,24 +35,40 @@ angular.module("afCourse").controller("afCourseLearnController",
             });
             $scope.userData = dataService.retrieveData();
             $scope.currentStep = 0;
+            $scope.lastAnswer = "";
 
-            $scope.answer = function (answer) {
+            $scope.getDraw = function () {
+                var code = $scope.courseData[$scope.currentStep].question.charCodeAt();
+                code = code.toString(16);
+                code = ("000" + code).slice(-5);
+                var path = 'ext-modules/afMainApp/data/' + code + '.svg'
+                return path;
+            }
+
+            $scope.answer = function () {
+                $scope.analyzing = true;
                 var canvas = document.getElementById('pwCanvasMain');
-                console.log('blah');
-                console.log(Tesseract.recognize(canvas, 'jpn'));
+                //canvas.getContext("2d").font = "30px Noto Sans";
+                //canvas.getContext("2d").fillText($scope.courseData[$scope.currentStep].question, 10, 50);
                 Tesseract.recognize(canvas, {
                     lang: 'jpn'
-                }).then(function (result) { console.log(result) });
-
-                if (answer == 'I KNOW!!') {
-                    $scope.userData.hiragana.push($scope.courseData[$scope.currentStep].question);
-                    dataService.saveData($scope.userData);
-                } else {
-                    $scope.lastAnswer = "False";
-                    $scope.courseData.push($scope.courseData[$scope.currentStep]);
-                }
-                $scope.currentStep++;
-
+                }).then(function (result) {
+                    $scope.analyzing = false;
+                    $scope.$apply(function () {
+                        if ($scope.courseData[$scope.currentStep].question == result.text.trim()) {
+                            $scope.lastAnswer = "RIGHTY OH";
+                            $scope.userData.hiragana.push($scope.courseData[$scope.currentStep].question);
+                            dataService.saveData($scope.userData);
+                            $scope.currentStep++;
+                        } else {
+                            $scope.lastAnswer = "YOU SUCK, DO AGAIN";
+                            //$scope.courseData.push($scope.courseData[$scope.currentStep]);
+                        }
+                    });
+                    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+                    
+                });
+                
             }
         }
     ]);
--- WebApplication1/WebApplication1/ext-modules/afMainApp/afMainAppDirective.js
+++ WebApplication1/WebApplication1/ext-modules/afMainApp/afMainAppDirective.js
@@ -10,3 +10,15 @@ angular.module("afMainApp").directive("afMainApp", function () {
         templateUrl: "ext-modules/afMainApp/afMainAppTemplate.html"
     };
 });
+
+angular.module("afMainApp").directive('backImg', function () {
+    return function (scope, element, attrs) {
+        attrs.$observe('backImg', function (value) {
+            element.css({
+                'background-image': 'url(' + value + ')',
+                'background-size': 'cover',
+                'opacity': 0.6
+            });
+        });
+    };
+});
